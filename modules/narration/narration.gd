adb80dd7f8ffd609f60e356b29bd8fbaa17a29d4
Sam <samuel.terrasviola@gmail.com>
2023-02-11 14:35:07 +0100

    fix merge

--- modules/narration/narration.gd
+++ modules/narration/narration.gd
@@ -10,6 +10,17 @@ enum AUDIO_LINE {Everyday , Exorcism, Intro, TooMuchTrigger}
 func _ready():
 	GameState.updated.connect(_on_game_state_updated)
 
+func _play_line(lineName : AUDIO_LINE):
+	var lineNameStr = AUDIO_LINE.keys()[lineName]
+	var subtitledAudioStream = load("res://modules/narration/Audio/"+lineNameStr+".tres")
+	if subtitledAudioStream != null:
+		narratorStreamPlayer.stream = subtitledAudioStream.Audio
+		narratorSubtitleLabel.text = subtitledAudioStream.SubtitleText
+	else:
+		var audioStream = load("res://modules/narration/Audio/"+lineNameStr+".wav")
+		narratorStreamPlayer.stream = audioStream
+	narratorStreamPlayer.play()
+
 func _on_game_state_updated():
 	if(GameState.KillCount == 1):
-		narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Everyday])
+		_play_line(AUDIO_LINE.Everyday)
