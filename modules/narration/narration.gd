f3876cd80220cb68b91385384e4bc1fc71d2f397
Rémi Monthiller <monthir@git.inpt.fr>
2023-02-11 14:08:32 +0100

    debug

--- modules/narration/narration.gd
+++ modules/narration/narration.gd
@@ -5,15 +5,9 @@ extends Control
 var narratorStreamPlayer : AudioStreamPlayer = get_node("AudioStreamPlayer")
 enum AUDIO_LINE {Everyday , Exorcism, Intro, TooMuchTrigger}
 
+func _ready():
+	GameState.updated.connect(_on_game_state_updated)
 
-
-func _on_timer_timeout():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Exorcism])
-
-
-func _on_game_state_trigger_intro_line():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Everyday])
-
-
-func _on_game_state_trigger_intro_line_too_much():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.TooMuchTrigger])
+func _on_game_state_updated():
+	if(GameState.KillCount == 1):
+		narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Everyday])
