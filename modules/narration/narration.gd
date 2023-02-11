00c8f6bfd1c946434d16154abf3b725f8bf66568
Sam <samuel.terrasviola@gmail.com>
2023-02-11 14:24:00 +0100

    subtitle first iteration

--- modules/narration/narration.gd
+++ modules/narration/narration.gd
@@ -3,17 +3,31 @@ extends Control
 
 @onready
 var narratorStreamPlayer : AudioStreamPlayer = get_node("AudioStreamPlayer")
+@onready
+var narratorSubtitleLabel : Label = get_node("Label")
 enum AUDIO_LINE {Everyday , Exorcism, Intro, TooMuchTrigger}
 
 
 
 func _on_timer_timeout():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Exorcism])
+	_play_line(AUDIO_LINE.Exorcism)
 
 
 func _on_game_state_trigger_intro_line():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Everyday])
+	_play_line(AUDIO_LINE.Everyday)
 
 
 func _on_game_state_trigger_intro_line_too_much():
-	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.TooMuchTrigger])
+	_play_line(AUDIO_LINE.TooMuchTrigger)
+
+
+func _play_line(lineName : AUDIO_LINE):
+	var lineNameStr = AUDIO_LINE.keys()[lineName]
+	var subtitledAudioStream = load("res://modules/narration/Audio/"+lineNameStr+".tres")
+	if subtitledAudioStream != null:
+		narratorStreamPlayer.stream = subtitledAudioStream.Audio
+		narratorSubtitleLabel.text = subtitledAudioStream.SubtitleText
+	else:
+		var audioStream = load("res://modules/narration/Audio/"+lineNameStr+".wav")
+		narratorStreamPlayer.stream = audioStream
+	narratorStreamPlayer.play()
