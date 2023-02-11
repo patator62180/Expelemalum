0d114ed6f50e963356b1c3e134293be6df26b35a
Sam <samuel.terrasviola@gmail.com>
2023-02-11 11:27:57 +0100

    gameState Integration + new line

--- modules/narration/narration.gd
+++ modules/narration/narration.gd
@@ -3,9 +3,17 @@ extends Control
 
 @onready
 var narratorStreamPlayer : AudioStreamPlayer = get_node("AudioStreamPlayer")
-enum AUDIO_LINE {Everyday , Exorcism, Intro}
+enum AUDIO_LINE {Everyday , Exorcism, Intro, TooMuchTrigger}
 
 
 
 func _on_timer_timeout():
 	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Exorcism])
+
+
+func _on_game_state_trigger_intro_line():
+	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.Everyday])
+
+
+func _on_game_state_trigger_intro_line_too_much():
+	narratorStreamPlayer._play_line(AUDIO_LINE.keys()[AUDIO_LINE.TooMuchTrigger])
