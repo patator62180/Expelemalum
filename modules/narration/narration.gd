5ba401ac4a84bf5e87d48b1b98e27d1c0879a6bb
ZeLatang <nathanbernard91@hotmail.fr>
2023-02-15 03:44:25 +0100

    ajout final narration with placeholders

--- modules/narration/narration.gd
+++ modules/narration/narration.gd
@@ -1,42 +1,38 @@
 extends Control
 
-const audio_source : String = "res://modules/narration/Audio/Audio - narrationDictionnary.csv"
 const subtitle_speed : float = 1.5
-
-var prompt_dict : Dictionary
-
-#Structure of indicators :
-# { INDICATORS.XX: [
-#		value,           #index 0,  the value of the indicator
-#		INDICATORS.XX,   
-#		{ PLAYTYPES.XX:
-#			{remaining_prompts : value
-#			prompts_array : [[]]  #by priority
-#			}         
-#		},
-#		]
-#	}
-		
-var indicators : Dictionary
 
 @onready
 var lastLineSpoken : Timer = $LastLineSpoken
-var queued_prompts : Array
-@onready
-var narratorStreamPlayer : AudioStreamPlayer = get_node("AudioStreamPlayer")
 @onready
 var narratorSubtitleLabel : Label = get_node("Label")
 @onready
 var animationPlayer : AnimationPlayer = get_node("AnimationPlayer")
+@onready
+var narratorStreamPlayer : AudioStreamPlayer = get_node("AudioStreamPlayer")
+const audio_source : String = "res://modules/narration/Audio/Audio - narrationDictionnary.csv"
 
 
-enum AUDIO_LINE {narration_timer_activity_high, narration_timer_activity_low, narration_timer_difficulty_high, narration_timer_difficulty_low, narration_timer_evilness_high, narration_timer_evilness_low, narration_timer_filler_1, narration_timer_filler_2, narration_timer_filler_3, narration_timer_progress_high, narration_timer_progress_low, narration_trigger_fewKills_exorcist, narration_trigger_fewKills_peasant, narration_trigger_firstKill_exorcist, narration_trigger_firstKill_peasant, narration_trigger_gameLaunch_1, narration_trigger_gameLaunch_2, narration_trigger_manaLack, narration_trigger_ww_intro}
+var prompt_dict : Dictionary
+var queued_prompts : Array
+enum AUDIO_LINE {narration_trigger_default_defeat, narration_trigger_default_victory, narration_trigger_gameLaunch_1, narration_trigger_firstKill_peasant, narration_trigger_manaLack, narration_trigger_firstKill_exorcist, narration_trigger_ww_intro}
 
-
-#Each one varies from -1 to 1. 0 is considered average gameState
+#See initiate indicators for structure
+var indicators : Dictionary
+#Each indicator value varies from -1 to 1. 0 is considered neutral 
 enum INDICATORS {curseActivity, curseEvilness, currentDifficulty, gameProgress, filler}
-
-enum PLAYTYPES {highIndicator, lowIndicator, noIndication}
+enum PLAYTYPES {highIndicator, lowIndicator, noIndication, victoryLowIndicator, defeatLowIndicator, victoryHighIndicator, defeatHighIndicator}
+const play_style_matching : Dictionary = {
+	INDICATORS.curseActivity : {
+		
+	}
+	
+}
+const play_style_default = "Balanced"
+const play_style_default_threshold = 1.5
+#if all indicators' absolute value is under the filler threshold, 
+# a filling prompt will be played.
+const filler_threshold : float = 0.2
 
 
 #INIT
@@ -51,8 +47,7 @@ func _ready():
 	
 	# Init
 	prompt_dict = parse_csv_database(audio_source)
-	initiate_indicators() #with every indicator to 0 
-	indicators[INDICATORS.filler][0] = 0.2 #setting indicator value
+ #setting indicator value
 	
 	# Début de la narration :
 #	lastLineSpoken.start() # Optionnel si play_line après
@@ -64,16 +59,16 @@ func initiate_indicators() :
 	#Make empty dictionnary
 	indicators = {}
 	for indicator in INDICATORS :
+		# [indicator value, indicator id, playtype dictionnary containing prompts]
 		indicators[INDICATORS[indicator]] = [0.0, INDICATORS[indicator], {}]
 		
 		for playType in PLAYTYPES :
 			indicators[INDICATORS[indicator]][2][PLAYTYPES[playType]] = {
 				"remaining_prompts" : 0,
+				"asked_prompts" : 0,
 				"prompts_array" : [[]]
 				}
 			
-			
-	
 	#fill it with prompt_dict computed values
 	for prompt in prompt_dict :
 		var prompt_array = prompt_dict[prompt]
@@ -82,8 +77,8 @@ func initiate_indicators() :
 		var indic = prompt_array["onIndicator"]
 		var playtyp = prompt_array["playType"]
 		
-		if (indic != null) and (playtyp != null) :
-			change_indicators_remaining_prompts(indic, playtyp, 1)
+		if (indic != null and indic >= 0) and (playtyp != null and indic >= 0) :
+			register_asked_prompt(indic, playtyp, 1)
 			var studied_array = indicators[indic][2][playtyp]["prompts_array"]
 			
 			if prompt_array["playOrderPriority"] > studied_array.size() :
@@ -91,6 +86,9 @@ func initiate_indicators() :
 			if studied_array[prompt_array["playOrderPriority"]-1] == null :
 				studied_array[prompt_array["playOrderPriority"]-1] = []
 			studied_array[prompt_array["playOrderPriority"]-1].append(prompt)
+	
+	#Set the filler threshold
+	indicators[INDICATORS.filler][0] = filler_threshold
 
 func parse_csv_database(
 	filename : String,
@@ -120,9 +118,11 @@ func parse_csv_database(
 				var value = line[column_index] as String
 
 				if value is String :
-					if toComputeIdentifier in column_headers[column_index] :
+					if value == "" :
+						value = -1
+					elif toComputeIdentifier in column_headers[column_index] :
 						
-						assert(value != "","the file contains a blanck cell on a computed column : " + audio_source)
+#						assert(value != "","the file contains a blanck cell on a computed column : " + audio_source)
 						
 						var expression = Expression.new()
 						expression.parse(value)
@@ -168,16 +168,15 @@ func play_situation_line(
 	indicator : INDICATORS,
 	playType : PLAYTYPES,
 	prompt_id : int = -1 #if -1 the func takes the highest priority at random
-	) -> bool :
+	) -> String :
 	
 	set_monitor("play line", false,"chosen situation :" + INDICATORS.keys()[indicator] + 	" - " + PLAYTYPES.keys()[playType] + "\n")
 
-	
+	var prompt_str : String
 	var enough_prompt_remaining : bool
 	
 	if get_indicators_remaining_prompts(indicator, playType) > 0 :
 		var prompts_array : Array = indicators[indicator][2][playType]["prompts_array"]
-		var prompt_str : String
 		
 		if prompts_array[0].size() == 1 :
 			prompt_str = prompts_array[0][0]
@@ -192,9 +191,9 @@ func play_situation_line(
 	else :
 		enough_prompt_remaining = false
 		
-	change_indicators_remaining_prompts(indicator,playType,-1)
+	register_asked_prompt(indicator,playType)
 	
-	return enough_prompt_remaining
+	return prompt_str
 
 func play_best_line_by_indicator() -> bool:
 	
@@ -215,25 +214,68 @@ func play_best_line_by_indicator() -> bool:
 		
 		if (studied_array[0] > 0) :
 			if studied_array[2][PLAYTYPES.highIndicator]["remaining_prompts"] > 0 :
-				error = not(play_situation_line(studied_array[1],PLAYTYPES.highIndicator))
+				error = (play_situation_line(studied_array[1],PLAYTYPES.highIndicator) == null)
 				prompt_found = true
 				
 			else :
-				change_indicators_remaining_prompts(studied_array[1],PLAYTYPES.highIndicator,-1)
+				register_asked_prompt(studied_array[1],PLAYTYPES.highIndicator)
 		else :
 			if studied_array[2][PLAYTYPES.lowIndicator]["remaining_prompts"] > 0 :
-				error = not(play_situation_line(studied_array[1],PLAYTYPES.lowIndicator))
+				error = (play_situation_line(studied_array[1],PLAYTYPES.lowIndicator) == null)
 				prompt_found = true
 	
 			else :
-				change_indicators_remaining_prompts(studied_array[1],PLAYTYPES.lowIndicator,-1)
+				register_asked_prompt(studied_array[1],PLAYTYPES.lowIndicator)
 				
 		iterator += 1
 
 	return prompt_found and not(error)
 
-func update_indicators():
+
+func play_best_ending_by_indicator(game_won : bool):
 	
+	var indicators_values = indicators.values()
+	indicators_values.sort_custom(
+		func(a,b): 
+			return max(a[2][PLAYTYPES.highIndicator]["asked_prompts"],a[2][PLAYTYPES.lowIndicator]["asked_prompts"]) > max(b[2][PLAYTYPES.highIndicator]["asked_prompts"],b[2][PLAYTYPES.lowIndicator]["asked_prompts"]) 
+	)
+	var selected_indic = indicators_values[0]
+	var selected_prompt_str : String
+	
+	if selected_indic[2][PLAYTYPES.highIndicator]["asked_prompts"] > play_style_default_threshold * selected_indic[2][PLAYTYPES.lowIndicator]["asked_prompts"] :
+		# High indicator !
+		if game_won :
+			selected_prompt_str = play_situation_line(selected_indic[1],PLAYTYPES.victoryHighIndicator)
+		else :
+			selected_prompt_str = play_situation_line(selected_indic[1],PLAYTYPES.defeatHighIndicator)
+	elif selected_indic[2][PLAYTYPES.lowIndicator]["asked_prompts"] > play_style_default_threshold * selected_indic[2][PLAYTYPES.highIndicator]["asked_prompts"] :
+		# Low indicator !
+		if game_won :
+			selected_prompt_str = play_situation_line(selected_indic[1],PLAYTYPES.victoryLowIndicator)
+		else :
+			selected_prompt_str = play_situation_line(selected_indic[1],PLAYTYPES.victoryLowIndicator)
+	
+	if selected_prompt_str == null or selected_prompt_str == "" :
+		if game_won :
+			_play_line_direct(AUDIO_LINE.narration_trigger_default_victory)
+			selected_prompt_str = "narration_trigger_default_victory"
+		else :
+			_play_line_direct(AUDIO_LINE.narration_trigger_default_defeat)
+			selected_prompt_str = "narration_trigger_default_defeat"
+	
+	GameState.set_player_narration_state(prompt_dict[selected_prompt_str]["specialDisplay"])
+
+
+func update_play_style():
+	var indicator_instance_count : int = 0
+	var instance_count_array : Array = []
+	
+	for i in INDICATORS :
+		for p in PLAYTYPES :
+			get_indicators_asked_prompts(INDICATORS[i], PLAYTYPES[p])
+			
+
+func update_indicators():
 	# EVILNESS
 	if ((GameState.peasant_kill_count + GameState.exorcist_kill_count) == 0) :
 		indicators[INDICATORS.curseEvilness][0] = 0.0
@@ -247,25 +289,25 @@ func update_indicators():
 	indicators[INDICATORS.gameProgress][0] = 2*(GameState.exorcist_kill_count / (GameState.exorcist_kill_count + GameState.remaining_exorcists_count)) - 1.0 #can be zero if world not init
 	
 	#ACTIVITY
-#	var currentDate : float = Time.get_ticks_msec()/1000
-#	if (GameState.curse_events.size() == 0) :
-#		indicators[INDICATORS.curseActivity][0] = -1.0
-#	else :
-#		while(GameState.curse_events[0] < currentDate - 20) :
-#			GameState.curse_events.remove_at(0)
-#		indicators[INDICATORS.curseActivity][0] = 2 *(GameState.curse_events.size() / 20) - 1.0
 	var number_of_swipes_done = GameState.get_updated_curse_events().size()
 	indicators[INDICATORS.curseActivity][0] = 2 *(GameState.curse_events.size() / GameState.curse_memory_duration) - 1.0
 	
 	
+	GameState.set_player_narration_state("yolo")
+	
+	#debug
 	set_monitor("update indicator")
 
-func change_indicators_remaining_prompts(
+func register_asked_prompt(
 	indicator : INDICATORS,
 	playtype : PLAYTYPES,
-	delta : int) :
-			
-	indicators[indicator][2][playtype]["remaining_prompts"] += delta
+	delta : int = -1) :
+	
+	var studied_dict = indicators[indicator][2][playtype]
+	
+	if (delta < 0 and studied_dict["remaining_prompts"] > 0) or (delta > 0) :
+		studied_dict["remaining_prompts"] += delta
+	studied_dict["asked_prompts"] += delta
 	
 func get_indicators_remaining_prompts(
 	indicator : INDICATORS,
@@ -273,6 +315,13 @@ func get_indicators_remaining_prompts(
 	) -> int :
 			
 	return indicators[indicator][2][playtype]["remaining_prompts"]
+
+func get_indicators_asked_prompts(
+	indicator : INDICATORS,
+	playtype : PLAYTYPES
+	) -> int :
+		
+	return indicators[indicator][2][playtype]["asked_prompts"]
 
 # PLAYER MANAGEMENT
 ############################################################################
@@ -331,12 +380,17 @@ func _on_last_line_spoken_timeout():
 	lastLineSpoken.stop()
 	play_best_line_by_indicator()
 
-func _on_game_change(old, new) :
-	match new :
+func _on_game_change(old) :
+	
+	print("game phase change to ", GameState.current_game_phase)
+	match GameState.current_game_phase :
 		GameState.GAME_PHASE.Intro :
 			pass
 		GameState.GAME_PHASE.Gameplay :
+			initiate_indicators() #with every indicator to 0 except filler
 			lastLineSpoken.start()
 		GameState.GAME_PHASE.Outro :
 			lastLineSpoken.stop()
+			queued_prompts.clear()
+			play_best_ending_by_indicator(GameState.game_won)
 
