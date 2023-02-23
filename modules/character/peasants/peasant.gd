6f3c78e0efae38513ad9bb6aba56f95d70f109b6
patator62180 <samuel.terrasviola@gmail.com>
2023-02-23 16:08:35 +0100

    Circle on feet
    add small circle on peasant feet when cursed to help readability

--- modules/character/peasants/peasant.gd
+++ modules/character/peasants/peasant.gd
@@ -8,8 +8,8 @@ signal metamorphosing
 signal unmetamorphosing
 signal metamorphosed
 signal unmetamorphosed
-
 signal cursed
+signal uncursed
 
 # interface
 
@@ -23,28 +23,19 @@ func metamorphose():
 		$TimerMetamorphose.start()
 		emit_signal("metamorphosing")
 
-func _on_timer_metamorphose_timeout():
-	is_metamorphosing = false
-	is_metamorphosed = true
-	for area in $Area2DCloseRange.get_overlapping_areas():
-		var character : Node2D = area.get_parent()
-		if character != self:
-			_on_character_got_at_close_range(character)
-	$TimerMetamorphosisDuration.start()
-	emit_signal("metamorphosed")
-
 func unmetamorphose():
 	if not is_dead and not is_dying:
 		is_metamorphosing = true
 		$TimerUnmetamorphose.start()
 		emit_signal("unmetamorphosing")
 
-func _on_timer_unmetamorphose_timeout():
-	is_metamorphosing = false
-	is_metamorphosed = false
-	if not has_metamorphosed:
-		has_metamorphosed = true
-	emit_signal("unmetamorphosed")
+func curse():
+	is_cursed = true
+	emit_signal("cursed")
+
+func uncurse():
+	is_cursed = false
+	emit_signal("uncursed")
 
 func die(killer_character : Node2D):
 	super.die(killer_character)
@@ -65,3 +56,20 @@ func _on_character_got_at_close_range_metamorphosed(_character : Node2D):
 
 func _on_timer_metamorphosis_duration_timeout():
 	unmetamorphose()
+
+func _on_timer_metamorphose_timeout():
+	is_metamorphosing = false
+	is_metamorphosed = true
+	for area in $Area2DCloseRange.get_overlapping_areas():
+		var character : Node2D = area.get_parent()
+		if character != self:
+			_on_character_got_at_close_range(character)
+	$TimerMetamorphosisDuration.start()
+	emit_signal("metamorphosed")
+
+func _on_timer_unmetamorphose_timeout():
+	is_metamorphosing = false
+	is_metamorphosed = false
+	if not has_metamorphosed:
+		has_metamorphosed = true
+	emit_signal("unmetamorphosed")
