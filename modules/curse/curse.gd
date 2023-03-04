ccd3740956a7d96e39b26178f967598ff9ed5a17
patator62180 <samuel.terrasviola@gmail.com>
2023-03-04 13:59:51 +0100

    adress feedbacks

--- modules/curse/curse.gd
+++ modules/curse/curse.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 #const var
-const TARGET_ANGLE_EPSILON = 0.1* TAU
-const SKULL_SPEED = 10 #pixel/second
+const TARGET_ANGLE_EPSILON : float = 0.1* TAU
+const SKULL_SPEED : float = 10 #pixel/second
 
 const ANIM_IDLE = "Skull_Idle"
 const ANIM_INTRO = "skull_intro"
@@ -11,11 +11,29 @@ const ANIM_OUTRO = "skull_outro"
 @export_node_path("Node2D") var INITIALLY_CURSED_CHARACTER : NodePath
 
 #ingame var
-var _cursed_character : Node2D = null
-var _cursable_character : Node2D = null
-var _controller_enabled : bool = false
+var cursed_character : Node2D = null:
+	get:
+		return cursed_character
+	set(character):
+		if cursed_character != null:
+			cursed_character.uncurse()
+			cursed_character.tree_exiting.disconnect(_oncursed_character_dead)
+		cursed_character = character
+		if cursed_character != null:
+			cursed_character.tree_exiting.connect(_oncursed_character_dead)
+			cursed_character.curse()
+			$Sfx/AudioStreamPlayer2DJump.play()		
+			GameState.on_curse(character)
+		cursable_character = null
 
-@onready var skull_sprite : Node2D = get_node("Path2D/PathFollow2D/Skull")
+var cursable_character : Node2D = null:
+	get:
+		return cursable_character
+	set(character):
+		cursable_character = character
+		$Pointer.target = cursable_character
+
+var _controller_enabled : bool = false
 
 func enable_controller(enable : bool, play_anim : bool = true):
 	_controller_enabled = enable
@@ -29,10 +47,10 @@ func enable_controller(enable : bool, play_anim : bool = true):
 
 func _ready():
 	enable_controller(false, false)
-	_curse(get_node(INITIALLY_CURSED_CHARACTER))
+	cursed_character = get_node(INITIALLY_CURSED_CHARACTER)
 	tree_exiting.connect(_on_tree_exiting)
 	$AnimationPlayerSkull.connect("animation_finished", _on_curse_animation_finished)
-	$CurvedLines.origin = skull_sprite
+	$CurvedLines.origin = $Path2D/PathFollow2D/Skull
 	$CurvedLines.target = $Pointer
 
 func _process(delta : float):
@@ -43,11 +61,11 @@ func _process(delta : float):
 func _input(event : InputEvent):
 	if _controller_enabled:
 		if event.is_action_released("curse") and not $Area2DMouse.mouse_in:
-			_try_curse(_cursable_character)
+			_try_curse(cursable_character)
 		if event.is_action_released("metamorphose") or event.is_action_released("curse") and $Area2DMouse.mouse_in:
-			if not _cursed_character.is_metamorphosed and not _cursed_character.is_metamorphosing:
-				GameState.on_metamorphose(_cursed_character)
-				_cursed_character.metamorphose()
+			if not cursed_character.is_metamorphosed and not cursed_character.is_metamorphosing:
+				GameState.on_metamorphose(cursed_character)
+				cursed_character.metamorphose()
 			else:
 				$AnimationPlayerForbidden.play("forbidden")
 				$Sfx/AudioStreamPlayerTransformForbidden.play()
@@ -59,46 +77,19 @@ func _on_tree_exiting():
 
 # internal
 
-func _curse(character : Node2D):
-	if _cursed_character != null:
-		_cursed_character.uncurse()
-		_cursed_character.tree_exiting.disconnect(_on_cursed_character_dead)
-	_cursed_character = character
-	_cursed_character.tree_exiting.connect(_on_cursed_character_dead)
-	_cursed_character.curse()
-	_free_cursable_character()
-	$Sfx/AudioStreamPlayer2DJump.play()
-
 func _try_curse(character : Node2D):
 	if character != null:
-		GameState.on_curse(character)
-		_curse(character)
+		cursed_character = character
 	else:
 		$AnimationPlayerForbidden.play("forbidden")
 		$Sfx/AudioStreamPlayerJumpForbidden.play()
 		$Path2D/PathFollow2D/Skull/Sprite2DForbidden/Sprite2DLeft.show()
 		$Path2D/PathFollow2D/Skull/Sprite2DForbidden/Sprite2DRight.hide()
 
-func _highlight_cursable_character(character : Node2D):
-	_free_cursable_character()
-	_cursable_character = character
-	
-	if _cursable_character != null:
-		if _cursable_character.is_queued_for_deletion():
-			_cursable_character = null
-	
-	$Pointer.target = _cursable_character
-
-func _free_cursable_character():
-	if _cursable_character != null:
-		if _cursable_character.is_queued_for_deletion():
-			_cursable_character = null
-	$Pointer.target = null
-
 func _update_cursable_character():
 	#if cursable go out of range, unhighlight
-	if _cursable_character != null and not _cursable_character.is_dead and not $CurseArea.overlaps_area(_cursable_character.get_node("Area2DBody")):
-		_free_cursable_character()
+	if cursable_character != null and not cursable_character.is_dead and not $CurseArea.overlaps_area(cursable_character.get_node("Area2DBody")):
+		cursable_character = null
 
 	var inputVector : Vector2 = _get_input_vector()
 	
@@ -106,38 +97,38 @@ func _update_cursable_character():
 	var cursable_candidates : Array = Array()
 	for area in overlapping_areas:
 		var character : Node2D = area.get_parent()
-		if character != _cursed_character:
-			var toCharacterVector : Vector2 = character.global_position - _cursed_character.global_position
+		if character != cursed_character:
+			var toCharacterVector : Vector2 = character.global_position - cursed_character.global_position
 			if abs(toCharacterVector.angle_to(inputVector)) < TARGET_ANGLE_EPSILON:
 				cursable_candidates.append(character)
 	
-	_highlight_cursable_character(_get_closest_character(cursable_candidates))
+	cursable_character = _get_closest_character(cursable_candidates)
 
-func _dist_to_cursable_character()-> float :
-	if _cursable_character == null:
+func _dist_tocursable_character()-> float :
+	if cursable_character == null:
 		return INF
-	return _cursable_character.global_position.distance_to(_cursed_character.global_position)
+	return cursable_character.global_position.distance_to(cursed_character.global_position)
 
 func _get_closest_character(characters : Array) -> Node2D:
 	var closest_character : Node2D = null
 	var closest_character_distance : float = INF
 	for character in characters:
-		if character != _cursed_character:
-			var character_distance : float = (character.global_position - _cursed_character.global_position).length()
+		if character != cursed_character:
+			var character_distance : float = (character.global_position - cursed_character.global_position).length()
 			if character_distance < closest_character_distance:
 				closest_character = character
 				closest_character_distance = character_distance
 	return closest_character
 	
 func _get_input_vector() -> Vector2:
-	var inputVector = (get_global_mouse_position()-skull_sprite.global_position)
+	var inputVector = (get_global_mouse_position()-$Path2D/PathFollow2D/Skull.global_position)
 	if inputVector == Vector2.ZERO:
 		return inputVector
 	return inputVector.normalized()
 
 func _update_position(delta : float):
-	if _cursed_character:
-		global_position += (_cursed_character.global_position - global_position) * delta * SKULL_SPEED
+	if cursed_character:
+		global_position += (cursed_character.global_position - global_position) * delta * SKULL_SPEED
 
 func _on_curse_animation_finished(anim_name : StringName):
 	if anim_name == ANIM_INTRO:
@@ -146,7 +137,7 @@ func _on_curse_animation_finished(anim_name : StringName):
 	if anim_name == ANIM_OUTRO:
 		queue_free()
 
-func _on_cursed_character_dead():
-	_cursed_character = null
+func _oncursed_character_dead():
+	cursed_character = null
 	enable_controller(false)
 	$AnimationPlayerSkull.play(ANIM_OUTRO)
